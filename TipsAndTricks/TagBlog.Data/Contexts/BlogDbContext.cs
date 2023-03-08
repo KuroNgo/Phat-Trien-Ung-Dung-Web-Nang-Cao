b10fb2c1036a6cc6920b97f734181905d6391805
Ngô Hoài Phong <81272162+KuroNgo@users.noreply.github.com>
2023-03-08 22:46:58 +0700

    uncomplete

--- TipsAndTricks/TagBlog.Data/Contexts/BlogDbContext.cs
+++ TipsAndTricks/TagBlog.Data/Contexts/BlogDbContext.cs
@@ -12,7 +12,8 @@ namespace TagBlog.Data.Contexts
         public DbSet<Tag> Tags { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-KRU45S6;Database=TagBlog;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-KRU45S6\\MSSQLSERVER01;Initial Catalog=TatBlog;Trusted_Connection=True;Integrated Security=True");
+            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-KRU45S6;Database=TagBlog;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True");
             //optionsBuilder.UseSqlServer(@"Server=DESKTOP-KRU45S6;Database=TagBlog;Trusted_Connection=True;MultipleActiveResultSets=True");
 
         }
@@ -21,5 +22,6 @@ namespace TagBlog.Data.Contexts
             modelBuilder.ApplyConfigurationsFromAssembly(
                 typeof(CategoryMap).Assembly);
         }
-    }
+      
+    }   
 }
---
+++ TipsAndTricks/TatBlog.WebApp/TatBlog.WebApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TagBlog.Data.Contexts;
+using TagBlog.Data.Seeders;
+using TagBlog.Services.Blogs;
+using TatBlog.WebApp.Extensions;
+namespace TatBlog.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            {
+                builder.ConfigureMvc()
+                       .ConfigureServices();
+            }
+
+            var app = builder.Build();
+            {
+                app.UseRequestPipeline();
+                app.UseBlogRoutes();
+                app.UseDataSeeder();
+            }
+
+            app.Run();
+        }
+    }
+}
